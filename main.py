5d4064c43ab733750b3277b7cbe54001aa059139
Martin Tomasi <martin.tomasi@gmail.com>
2014-12-26 12:19:18 +0100

    added caching of the results, REST API works now

--- main.py
+++ main.py
@@ -1,30 +1,33 @@
 from flask import Flask
 from private_data import API_KEY
 from urllib.request import urlopen
-from urllib.parse import urlencode, quote_plus
+from urllib.parse import urlencode
 import json
+import random
 
 def filter_empty(dictionary):
+    """Removes all key-value pairs where the value is empty."""
     return dict((k, v) for k, v in dictionary.items() if v)
 
 
 class GBApi():
+    """A representation of the Giant Bomb API."""
     def __init__(self, api_key):
         self.api_key = api_key
         self.video_types = {}
-        types_url = self.append_api_key('http://www.giantbomb.com/api/video_types/?format=json')
-        types_data = GBApi.load_into_dict(types_url)
+        base_url = 'http://www.giantbomb.com/api/video_types/?'
+        params = {'format': 'json', 'api_key': self.api_key}
+        types_url = base_url + urlencode(params)
+        types_data = self.load_into_dict(types_url)
         for _type in types_data['results']:
             t_name = _type['name'].lower().replace(' ', '_')
             t_id = _type['id']
             self.video_types[t_name] = t_id
 
         self.video_counts = {}
-    
-    def append_api_key(self, url):
-        return url + '&api_key=' + self.api_key
-        
-    def load_into_dict(url):
+        self.cache = {}
+
+    def load_into_dict(self, url):
         reply = urlopen(url)
         json_string = reply.readall().decode()
         return json.loads(json_string)
@@ -43,7 +46,7 @@ class GBApi():
         video_type = self.video_types[category_name]
         offset = kwargs.get('offset')
         limit = kwargs.get('limit')
-  
+
         filters = filter_empty({'id': video_id, 'name': name,
                                 'publish_date': date, 'video_type': video_type})
         filter_string = ''
@@ -55,8 +58,8 @@ class GBApi():
 
         params = urlencode(args)
         url = 'http://www.giantbomb.com/api/videos/?%s' % params
-        data = GBApi.load_into_dict(url)
-        
+        data = self.load_into_dict(url)
+
         count = data['number_of_total_results']
         if category_name:
             self.video_counts[category_name] = count
@@ -66,45 +69,46 @@ class GBApi():
         return data['results']
 
     def all_videos(self, category_name):
-        as a lower case string, with spaces replaced by
-        underscores.""
+        """write me"""
         if category_name and category_name not in self.video_types:
-            raise ValueError('invalid video type: %s' % category)
+            raise ValueError('invalid video type: %s' % category_name)
+        if not category_name:
+            category_name = 'all'
+
+        if category_name in self.cache:
+            return self.cache[category_name]
 
         count = None
-        if category_name:
-            count = self.video_counts.get(category_name)
-        else:
-            count = self.video_counts.get('all')
-        
-        if not count:
-            if category_name:
-                self.videos(video_type=category_name)
-            else:
-                self.videos()
+        cat = self.video_counts.get(category_name)
+        if not cat:
+            self.videos(video_type=category_name)
+            cat = self.video_counts.get(category_name)
+        count = cat
 
-        if category_name:
-            count = self.video_counts.get(category_name)
-        else:
-            count = self.video_counts.get('all')
-        
         all_results = []
         for offset in range(0, count, 100):
             if category_name:
                 result = self.videos(offset=offset, video_type=category_name)
             else:
                 result = self.videos(offset=offset)
+            print("Fetching page with offset ", offset)
             all_results.extend(result)
 
+        self.cache[category_name] = all_results
         return all_results
-        
-    
+
+
+api = GBApi(API_KEY)
 app = Flask(__name__)
-@app.route('/')
-def hello():
-    # TODO return a redirect to a random video in a given category
-    return '<h1>Hello World!</h1>'
+
+@app.route('/<category>')
+def hello(category):
+    videos = api.all_videos(category)
+    idx = random.randint(0, len(videos))
+    print(idx)
+    url = videos[idx]['site_detail_url']
+    return '<a href="%s">go!</a>' % url
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
